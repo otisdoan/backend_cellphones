3c1c6dd983d07eb959270cb34ca3d4e9ca70958e
otisdoan <ledoanhieu12a6@gmail.com>
2025-07-23 15:45:32 +0700

    add more cors

--- src/index.js
+++ src/index.js
@@ -7,6 +7,14 @@ const allRoute = require("./routes/index");
 const cors = require("cors");
 
 app.use(cors());
+app.use(
+  cors({
+    origin: "https://frontend-cellphones.vercel.app", // đổi thành URL thật FE Vercel của bạn
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
