6cc1dc4c229cffcb1e5f33a3fc232a1e665a0016
otisdoan <ledoanhieu12a6@gmail.com>
2025-07-25 19:09:52 +0700

    set url frontend

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const cors = require("cors");
 app.use(cors());
 app.use(
   cors({
-    origin: "https://frontend-cellphones.vercel.app", // đổi thành URL thật FE Vercel của bạn
+    origin: "http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
