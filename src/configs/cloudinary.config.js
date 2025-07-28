b26632ee68beab327bc4b0aa6aff16a2049b7629
otisdoan <ledoanhieu12a6@gmail.com>
2025-07-28 22:12:20 +0700

    fix key

--- src/configs/cloudinary.config.js
+++ src/configs/cloudinary.config.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.API_KEy,
+  api_key: process.env.API_KEY,
   api_secret: process.env.API_SECRET,
 });
 
