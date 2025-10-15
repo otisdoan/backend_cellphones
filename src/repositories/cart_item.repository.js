93a18449f47fea2622438ee5b3d55367a018266e
otisdoan <ledoanhieu12a6@gmail.com>
2025-10-15 07:56:43 +0700

    update query cart_items

--- src/repositories/cart_item.repository.js
+++ src/repositories/cart_item.repository.js
@@ -1,11 +1,23 @@
 const CartItem = require("../models/cart_item.model");
+const sequelize = require("../configs/database.config");
+const { QueryTypes } = require("sequelize");
 
 const findAllCartItemRepository = async () => {
   return await CartItem.findAll();
 };
 
 const findCartItemByIdRepository = async (id) => {
-  return await CartItem.findAll({ where: { user_id: id } });
+  return await sequelize.query(
+    ` select pv.*, ci.quantity
+      from product_variants pv
+      join cart_items ci on pv.product_id = ci.product_id
+      where ci.user_id = :id
+    `,
+    {
+      type: QueryTypes.SELECT,
+      replacements: { id },
+    }
+  );
 };
 
 const createCartItemRepository = async (payload) => {
