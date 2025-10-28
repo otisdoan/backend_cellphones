158425bd5b68fabae950888c3fc77d830a37a0cc
otisdoan <ledoanhieu12a6@gmail.com>
2025-10-28 16:30:26 +0700

    fix api checkout

--- src/controllers/payment.controller.js
+++ src/controllers/payment.controller.js
@@ -1,15 +1,8 @@
 const payos = require("../configs/payos.config");
 
 const createPayment = async (req, res) => {
-  const paymentData = {
-    orderCode: Date.now(),
-    amount: 2000,
-    description: "Thanh toán đơn hàng",
-    returnUrl: "http://localhost:3000/success",
-    cancelUrl: "http://localhost:3000/cancel",
-  };
   try {
-    const paymentLink = await payos.paymentRequests.create(paymentData);
+    const paymentLink = await payos.paymentRequests.create(req.body);
     res.json({
       qrCode: paymentLink.qrCode,
       checkoutUrl: paymentLink.checkoutUrl,
